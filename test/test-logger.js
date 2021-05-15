0d5ff601447c54fb910b247992a71b579db8f476
Yogendra Rampuria (Yogi) <yogendrarampuria@gmail.com>
2021-05-15 16:23:01 +0800

    Added test to check interpolation

--- test/test-logger.js
+++ test/test-logger.js
@@ -10,3 +10,15 @@ logger.log('error', "127.0.0.1 - there's no place like home");
 logger.info("127.0.0.1 - there's no place like home");
 logger.warn("127.0.0.1 - there's no place like home");
 logger.error("127.0.0.1 - there's no place like home");
+
+
+let place = { home: "127.0.0.1"}
+logger.log('silly', "%s - there's no place like home", place.home);
+logger.log('debug', "%s - there's no place like home", place.home);
+logger.log('verbose', "%s - there's no place like home", place.home);
+logger.log('info', "%s - there's no place like home", place.home);
+logger.log('warn', "%s - there's no place like home", place.home);
+logger.log('error', "%s - there's no place like home", place.home);
+logger.info("%s - there's no place like home", place.home);
+logger.warn("%s - there's no place like home", place.home);
+logger.error("%s - there's no place like home", place.home);
