624c43c982507c5497c9427c25ea1c445b4737ed
Yogendra Rampuria (Yogi) <yogendrarampuria@gmail.com>
2021-05-15 16:20:39 +0800

    test-logger

---
+++ test/test-logger.js
@@ -0,0 +1,12 @@
+
+import logger from '../lib/logger.js';
+
+logger.log('silly', "127.0.0.1 - there's no place like home");
+logger.log('debug', "127.0.0.1 - there's no place like home");
+logger.log('verbose', "127.0.0.1 - there's no place like home");
+logger.log('info', "127.0.0.1 - there's no place like home");
+logger.log('warn', "127.0.0.1 - there's no place like home");
+logger.log('error', "127.0.0.1 - there's no place like home");
+logger.info("127.0.0.1 - there's no place like home");
+logger.warn("127.0.0.1 - there's no place like home");
+logger.error("127.0.0.1 - there's no place like home");
