80d8fa56051c21f00c58d5e1c7829db08acea845
Yogendra Rampuria (Yogi) <yogendrarampuria@gmail.com>
2021-05-22 03:04:04 +0800

    Added debugging support

--- bin/cmgr.js
+++ bin/cmgr.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
-import { runFolder } from '../index.js';
+import { runFolder } from "../index.js";
+import waclient from "../lib/waclient.js";
+import logger from "../lib/logger.js";
 
-let folder = process.argv[2] || './campaign';
+let folder = process.argv[2] || "./campaign";
 
-runFolder(folder);
-
+logger.debug("Creating WAclient");
+let client = await waclient({
+  puppeteer: {
+    headless: process.env.CMGR_DEBUG=1
+  }
+});
+let reports = await runFolder(folder);
+logger.debug("Finished Running Folder");
+await client.destroy()
+logger.debug("Bye!");
